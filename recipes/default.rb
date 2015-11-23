a746c8a53d86acf79984cffc899a0eba8d9c31e7
Peter Halliday <pghalliday@gmail.com>
2015-11-23 17:01:51 +0100

    don't need ssl for logstash - everything will be inside the private network

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,17 +1,10 @@
 version = node['formatron_logstash']['version']
 checksum = node['formatron_logstash']['checksum']
 port = node['formatron_logstash']['port']
-certificate = node['formatron_logstash']['certificate']
-private_key = node['formatron_logstash']['private_key']
 
 cache = Chef::Config[:file_cache_path]
 deb = File.join cache, 'logstash.deb' 
 deb_url = "https://download.elastic.co/logstash/logstash/packages/debian/logstash_#{version}.deb"
-
-certificates_dir = '/etc/pki/tls/certs'
-certificate_path = File.join certificates_dir, 'beats.crt'
-private_keys_dir = '/etc/pki/tls/keys'
-private_key_path = File.join private_keys_dir, 'beats.key'
 
 remote_file deb do
   source deb_url
@@ -25,29 +18,9 @@ dpkg_package 'logstash' do
   notifies :restart, 'service[logstash]', :delayed
 end
 
-directory certificates_dir do
-  recursive true
-end
-
-file certificate_path do
-  content certificate
-  notifies :restart, 'service[logstash]', :delayed
-end
-
-directory private_keys_dir do
-  recursive true
-end
-
-file private_key_path do
-  content private_key
-  notifies :restart, 'service[logstash]', :delayed
-end
-
 template '/etc/logstash/conf.d/01-beats-input.conf' do
   variables(
-    port: port,
-    certificate: certificate_path,
-    private_key: private_key_path
+    port: port
   )
   notifies :restart, 'service[logstash]', :delayed
 end
